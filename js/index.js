d41b00bc6624ce291ffaf4a23c5736025d638bfd
avoRaDev <95705547+avoRaDev@users.noreply.github.com>
2022-03-16 18:23:02 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ const scene = new THREE.Scene();
 // set up renderer
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffffff, 1); // background (color, alpha)
+renderer.setClearColor(0xffffff, 0); // background (color, alpha)
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer. domElement);
 
 // set up camera
@@ -16,11 +17,8 @@ const camera = new THREE.PerspectiveCamera(
    75, // FOV
    window.innerWidth / window.innerHeight, // aspect ratio
    0.1, // near plane
-   1000 // far plane
+   100 // far plane
 );
-
-// set up orbit controls (optional, and broken)
-// const controls = new OrbitControls(camera, renderer.domElement);
 
 // set up texture loader
 const textureLoader = new THREE.TextureLoader();
@@ -28,45 +26,42 @@ const textureLoader = new THREE.TextureLoader();
 // textures
 const testTile = textureLoader.load('./textures/testTile.jpg');
 const testTileNormal = textureLoader.load('./textures/testTileNormal.jpg');
-const bumpyNormal = textureLoader.load('./textures/bumpyNormal.jpg');
+
 // ----------------------------------
 // ENVOIROMENT SETUP
 // ----------------------------------
 
 // set camera offset
-const cameraOffset = {x: 0, y: 5, z: 10}; // {x: 0, y: 2, z: 10};
+const cameraOffset = {x: 0, y: 1, z: 5}; // {x: 0, y: 2, z: 10};
 camera.position.set(cameraOffset.x, cameraOffset.y, cameraOffset.z); // change location of the camera
 
 // set up lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
 scene.add(ambientLight);
 
-const dirLight = new THREE.DirectionalLight(0xf7f7f7, 0.5);
-dirLight.position.set(20, 0, 20);
-scene.add(dirLight);
-
-// set up shading (WIP)
+const pointLight = new THREE.PointLight(0xf7f7f7, 0.5, 100, 2);
+pointLight.position.set(20, 0, 20);
+pointLight.castShadows = true;
+scene.add(pointLight);
 
 // ----------------------------------
 // MAIN MODELS
 // ----------------------------------
 
 // floor plane
-const mapWidth = 50;
-const mapHeight = 50;
+const mapWidth = 80;
+const mapHeight = 80;
 
 const plane = new THREE.Mesh(
    new THREE.PlaneBufferGeometry(mapWidth, mapHeight),
-   new THREE.MeshStandardMaterial({
-      color: 0x00ff00,
-      normalMap: bumpyNormal
-   })
+   new THREE.MeshStandardMaterial({color: 0x00ff00})
 );
 plane.rotation.x = -(Math.PI / 2);
+plane.recieveShadow = true;
 scene.add(plane);
 
 // player model
-const player = new PlayerModel(1, {
+const player = new PlayerModel(0.5, {
    cameraOffset: {
       x: cameraOffset.x,
       y: cameraOffset.y,
@@ -76,50 +71,17 @@ const player = new PlayerModel(1, {
 scene.add(player.group);
 
 // tree models
-// const trees = [];
-// for (let i = 0; i < 10; i++) {
-//    generateTree(
-//       getRandFloat(0.5,2),
-//       {
-//          x: getRandFloat(-25,25),
-//          z: getRandFloat(-25,25)
-//       }
-//    );
-// }
+const trees = [];
+// parameters for generateForest: amount, minSize, maxSize, minPos, maxPos
+generateForest(50, 0.5, 1.5, -25, 25);
 
+/* (uncomment for debugging)
 // ----------------------------------
 // HELPER MODELS
 // ----------------------------------
 
-// look into emissive and emissiveIntensity for lambert
-
-// debuugging
+// debuug menu
 const debug = document.querySelector('#debug');
-
-// test model
-const testModel = new TestModel(2, 0.5);
-testModel.tp(0, 2, 0);
-scene.add(testModel.group);
-
-// normal test model
-const testModel2 = new THREE.Mesh(
-   new THREE.BoxGeometry(1, 1, 1),
-   new THREE.MeshNormalMaterial()
-);
-testModel2.position.set (-4, 1, 0);
-scene.add(testModel2);
-
-// test plane (texturing)
-const testPlane = new THREE.Mesh(
-   new THREE.PlaneBufferGeometry(4,4),
-   new THREE.MeshStandardMaterial({
-      side: THREE.DoubleSide,
-      map: testTile,
-      normalMap: testTileNormal
-   })
-);
-testPlane.position.set(4, 2, 0);
-scene.add(testPlane);
 
 // grid
 const gridHelper = new THREE.GridHelper(50, 50); // size, divisions
@@ -130,6 +92,45 @@ const arrowHelper = new ArrowHelperModel();
 scene.add(arrowHelper.group);
 
 // ----------------------------------
+// TEST MODELS
+// ----------------------------------
+
+// l-piece test model
+const testModel = new TestModel(2, 0.5);
+testModel.tp(0, 2, 0);
+scene.add(testModel.group);
+
+// normal-map test model
+const testModel2 = new TestModel(1, 1);
+testModel2.tp(-4, 1, 0);
+scene.add(testModel2.group);
+
+// plane test model
+const testModel3 = new TestModel(3, 4, new THREE.MeshStandardMaterial({
+   side: THREE.DoubleSide,
+   map: testTile,
+   normalMap: testTileNormal
+}));
+testModel3.tp(4, 2, 0);
+scene.add(testModel3.group);
+
+// test model manipulation (c + p this into the render function to use)
+   // debugging
+   updateDebug();
+	
+	// rotate test l-piece
+   testModel.spin('x', 0.01);
+   testModel.spin('y', 0.01);
+   testModel.spin('z', 0.01);
+   
+   // rotate test cube
+   testModel2.spin('y', 0.01);
+   
+   // rotate test plane
+   testModel3.spin('x', 0.01);
+*/
+
+// ----------------------------------
 // RENDER LOOP
 // ----------------------------------
 
@@ -138,11 +139,29 @@ function render() {
    // render loop
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
-	
-	// orbit control (comment out if orbit controls wont be in use)
-   // controls.update();
-	
-	// debugging
+   
+   // update player information
+   player.update();
+   
+   // movement handler
+   Object.keys(player.movementFlag).forEach((key) => {
+      if (player.movementFlag[key]) {
+         player.movementHandler(key);
+      }
+   });
+   
+   // collision detection (WORK ON)
+}
+
+// begin rendering
+render();
+
+// ----------------------------------
+// UTILITY FUNCTIONS & EVENT LISTENERS
+// ----------------------------------
+
+// debug menu
+function updateDebug() {
 	debug.innerHTML = (`
 	   x-pos: ${player.group.position.x} <br>
 	   y-pos: ${player.group.position.y} <br>
@@ -157,34 +176,7 @@ function render() {
 	   cFC-y: ${player.cameraFollowCordinate.y} <br>
 	   cFC-z: ${player.cameraFollowCordinate.z} <br>
 	`);
-	
-	// rotate testPiece
-   testModel.spin('x', 0.01);
-   testModel.spin('y', 0.01);
-   testModel.spin('z', 0.01);
-   
-   // rotate testPlane
-   testPlane.rotation.x += 0.01;
-   
-   // update player information
-   player.update();
-   
-   // movement handler
-   Object.keys(player.movementFlag).forEach((key) => {
-      if (player.movementFlag[key]) {
-         player.movementHandler(key);
-      }
-   });
-   
-   // collision handler (WORK ON)
 }
-
-// begin rendering
-render();
-
-// ----------------------------------
-// UTILITY FUNCTIONS & EVENT LISTENERS
-// ----------------------------------
 
 // get random number
 function getRandFloat(min, max) {
